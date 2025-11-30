ec0e9f570495528de03f116b6543e30a2b17ffd2
Timoteo Pasinato <wpasinato@bna.com.ar>
2025-11-30 20:41:46 -0300

    ej03-3 reubico funcion useProductFilter.ts
    Cambio el file a .ts porque es un hook

--- projects/ejercicios/03-ejercicio-tienda/src/routes/products/index.tsx
+++ projects/ejercicios/03-ejercicio-tienda/src/routes/products/index.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from '@tanstack/react-router';
 import { useQuery } from '@tanstack/react-query';
 import { productsApi } from '../../services/api';
 import { ProductCard } from '../../components/ProductCard';
-import { useProductFilters } from '../../components/useProductsFilters';
+import { useProductFilters } from '../../services/useProductsFilters';
 
 export const Route = createFileRoute('/products/')({
   component: ProductsComponent,
@@ -24,7 +24,7 @@ function ProductsComponent() {
         resetFilters
     } = useProductFilters(productsToFilter);
 
-    
+
   if (isLoading) {
     return (
       <div className="flex justify-center items-center min-h-[400px]">
--- projects/ejercicios/03-ejercicio-tienda/src/services/useProductsFilters.ts
+++ projects/ejercicios/03-ejercicio-tienda/src/services/useProductsFilters.ts
@@ -7,22 +7,11 @@ export const useProductFilters = (products: Product[] | undefined | null) => {
     
     // 1. ESTADO LOCAL (Inicializado a 0 y valor máximo fijo)
     // Usamos valores fijos para que los Hooks se llamen siempre.
-    const [maxPrice, setMaxPrice] = useState<number | string>(9999999); 
-    const [minPrice, setMinPrice] = useState<number | string>(0);
+    const [maxPrice, setMaxPrice] = useState<number | string>(''); 
+    const [minPrice, setMinPrice] = useState<number | string>('');
     const [searchTerm, setSearchTerm] = useState('');
 
     const originalRangeRef = useRef({ min: '', max: '' });
-
-    // 2. EFECTO: Actualizar min/max solo cuando los productos lleguen
-    useEffect(() => {
-        if (products && products.length > 0) {
-            const calculatedMin = Math.min(...products.map(p => p.price));
-            const calculatedMax = Math.max(...products.map(p => p.price));
-            
-            setMinPrice(calculatedMin); // Actualiza el estado con el valor real
-            setMaxPrice(calculatedMax); // Actualiza el estado con el valor real
-        }
-    }, [products]); // 👈 Dependencia: Solo se ejecuta la primera vez que 'products' llega o cambia
 
     const resetFilters = () => {
         setMinPrice(originalRangeRef.current.min); // Precio mínimo original
