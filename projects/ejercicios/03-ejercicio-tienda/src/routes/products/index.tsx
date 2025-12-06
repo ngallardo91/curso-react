ddbac9e5c75be35cf2f852486a53112b415640a9
MarceloHarp <elkuna10@gmail.com>
2025-12-06 20:44:24 -0300

    paginacion

--- projects/ejercicios/03-ejercicio-tienda/src/routes/products/index.tsx
+++ projects/ejercicios/03-ejercicio-tienda/src/routes/products/index.tsx
@@ -5,7 +5,7 @@ import { ProductCard } from '../../components/ProductCard';
 
 import ComponentFilter from '../../components/ComponentFilter';
 import type { PriceFilter } from '../../components/ComponentFilter';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ProductSkeleton } from '../../components/ProductSkeleton';
 import ErrorMsg from '../../components/ErrorMsg';
 
@@ -27,6 +27,9 @@ function ProductsComponent() {
     sortBy: '',
   });
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
   const filteredProducts = (products ?? []).filter((p) => {
     const matchesSearch =
       !filter.searchTerm ||
@@ -43,15 +46,15 @@ function ProductsComponent() {
 
   /* El ordenamiento */
   const sortedProducts = [...filteredProducts].sort((a, b) => {
-  if (filter.sortBy === "price-asc") return a.price - b.price;
-  if (filter.sortBy === "price-desc") return b.price - a.price;
- if (filter.sortBy === "best-rated") return b.rating.rate - a.rating.rate;
-  if (filter.sortBy === "most-reviews") return b.rating.count - a.rating.count;
-  return 0;
-});
-  
+    if (filter.sortBy === "price-asc") return a.price - b.price;
+    if (filter.sortBy === "price-desc") return b.price - a.price;
+    if (filter.sortBy === "best-rated") return b.rating.rate - a.rating.rate;
+    if (filter.sortBy === "most-reviews") return b.rating.count - a.rating.count;
+    return 0;
+  });
 
-{/*
+
+  {/*
   if (isLoading) {
     return (
       <div className="flex justify-center items-center min-h-[400px]">
@@ -60,22 +63,38 @@ function ProductsComponent() {
     );
   }
 */}
-if (isLoading) {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold text-gray-900 mb-6">
-        Todos los Productos
-      </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {Array.from({ length: 8 }).map((_, i) => (
-          <ProductSkeleton key={i} />
-        ))}
-      </div>
-    </div>
+  // realizo la paginaacion 
+  const totalProducts = sortedProducts.length;
+  const totalPages = Math.ceil(totalProducts / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const paginatedProducts = sortedProducts.slice(
+    startIndex,
+    startIndex + itemsPerPage
   );
-}
-{/*
+
+  // Si modifca un filtro, vuelve a la primera página
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [filter]);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold text-gray-900 mb-6">
+          Todos los Productos
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {Array.from({ length: 8 }).map((_, i) => (
+            <ProductSkeleton key={i} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+  {/*
   if (error) {
     return (
       <div className="text-center text-red-600 py-8">
@@ -85,10 +104,10 @@ if (isLoading) {
   }
 */}
   if (error) {
-    return  <ErrorMsg message="Error al cargar los productos" />
+    return <ErrorMsg message="Error al cargar los productos" />
   }
 
-  
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900 mb-6">
@@ -104,8 +123,8 @@ if (isLoading) {
         />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {sortedProducts.length > 0 ? (
-          sortedProducts.map((product) => (
+        {paginatedProducts.length > 0 ? (
+          paginatedProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))
         ) : (
@@ -114,6 +133,28 @@ if (isLoading) {
           </p>
         )}
       </div>
+      {totalPages > 1 && (
+        <div className="flex justify-center items-center gap-4 mt-8">
+          <button
+            className="px-3 py-2 bg-gray-200 rounded disabled:opacity-50"
+            onClick={() => setCurrentPage((p) => p - 1)}
+            disabled={currentPage === 1}
+          >
+            ← Anterior
+          </button>
+
+          <span className="font-medium">
+            Página {currentPage} de {totalPages}
+          </span>
+          <button
+            className="px-3 py-2 bg-gray-200 rounded disabled:opacity-50"
+            onClick={() => setCurrentPage((p) => p + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Siguiente →
+          </button>
+        </div>
+      )}
     </div>
   );
 }
